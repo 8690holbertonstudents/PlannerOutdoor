66358c1e828e5e26aa84517c4a97e41400d28775
8690holbertonstudents <8690@holbertonstudents.com>
2024-11-13 15:42:23 +0100

    PlannerOutdoor - Populate Homeview - Other minor changes

--- po_app/src/components/PageFooter.vue
+++ po_app/src/components/PageFooter.vue
@@ -1,6 +1,31 @@
 <template>
-  <footer>
-    <div id="footer-text">PlannerOutdoor portfolio project - November 2024</div>
+  <footer class="footer-container">
+    <div class="footer-item-left">
+      <a href="https://www.holbertonschool.fr/" target="_blank">
+        <img src="/images/seahorse_white.png" alt="holberton" />
+      </a>
+    </div>
+    <div class="footer-item-center">
+      <p>PlannerOutdoor portfolio project - November 2024</p>
+    </div>
+    <div class="footer-item-right">
+      <div class="link-container">
+        <a
+          href="https://github.com/8690holbertonstudents/PlannerOutdoor"
+          target="_blank"
+          class="link-item"
+        >
+          <img src="/images/github.png" alt="github project" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emmanuel-mg197274470/"
+          target="_blank"
+          class="link-item"
+        >
+          <img src="/images/linkedin.png" alt="linkedin" />
+        </a>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -11,18 +36,52 @@ export default {
 </script>
 
 <style scoped>
-footer {
+.footer-container {
   display: flex;
+  flex-direction: row;
   justify-content: center;
+  align-items: center;
   height: var(--header-footer-height);
   width: var(--header-footer-width);
   background-color: var(--color-header-footer);
   color: var(--color-white);
 }
-#footer-text {
-  display: flex;
-  align-items: center;
+
+.footer-item-left {
+  flex-basis: 5%;
+}
+
+.footer-item-left img {
+  height: 1.5rem;
+  width: 1.5rem;
+  padding-left: 1rem;
+}
+
+.footer-item-center {
+  flex-basis: 90%;
+  text-align: center;
   font-size: var(--font-size-small);
   font-weight: var(--font-weight-bold);
+}
+
+.footer-item-right {
+  flex-basis: 5%;
+}
+
+.footer-item-right img {
+  height: 1.5rem;
+  width: 1.5rem;
+  padding-right: 1rem;
+}
+
+.link-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.link-item {
+  margin: 0 0.5rem;
 }
 </style>
--- po_app/src/views/HomeView.vue
+++ po_app/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-container">
-    <div class="title-item">
-      <h1>Welcome to PlannerOutdoor !</h1>
+    <div class="home-item">
+      <h2>Welcome to PlannerOutdoor !</h2>
       <h3>
         This project enables users with an account to plan their outdoor
         activities, taking into account weather conditions and any allergies
@@ -11,7 +11,91 @@
         This project is not yet complete, and unfortunately not all features
         have been implemented.
       </h3>
-      <h2>Work in progress !!!</h2>
+    </div>
+    <div class="home-item">
+      <h2>Core technologies</h2>
+      <div class="logo-container">
+        <a
+          href="https://www.djangoproject.com/"
+          title="Django framework"
+          target="_blank"
+          class="logo-item"
+        >
+          <img src="images/django.png" alt="django" />
+        </a>
+        <a
+          href="https://www.django-rest-framework.org/"
+          title="Django rest framework"
+          target="_blank"
+          class="logo-item"
+        >
+          <img src="images/django_rest_framework.png" alt="django-drf" />
+        </a>
+        <a
+          href="https://vuejs.org/"
+          title="Vue.js framework"
+          target="_blank"
+          class="logo-item"
+        >
+          <img src="images/vue_js.png" alt="vue.js" />
+        </a>
+        <a
+          href="https://www.mysql.com/"
+          title="Mysql database"
+          target="_blank"
+          class="logo-item"
+        >
+          <img src="images/mysql_logo.png" alt="mysql" />
+        </a>
+        <a
+          href="https://openweathermap.org/"
+          title="Openweather API"
+          target="_blank"
+          class="logo-item"
+        >
+          <img src="images/openweather.png" alt="openweather" />
+        </a>
+      </div>
+    </div>
+    <div class="home-item">
+      <h2>API documentation & administration</h2>
+      <div class="api-container">
+        <a
+          href="http://localhost:8020/swagger/"
+          target="_blank"
+          class="api-item"
+        >
+          <img
+            src="/images/screen_capture_api_swagger.png"
+            title="Swagger documentation"
+            alt="swagger"
+          />
+        </a>
+        <a
+          href="http://localhost:8020/redoc/"
+          title="Redoc documentation"
+          target="_blank"
+          class="api-item"
+        >
+          <img src="/images/screen_capture_api_redoc.png" alt="redoc" />
+        </a>
+        <a
+          href="http://localhost:8020/admin/"
+          title="Django admin console"
+          target="_blank"
+          class="api-item"
+        >
+          <img src="/images/screen_capture_api_django-admin.png" alt="admin" />
+        </a>
+      </div>
+    </div>
+    <div class="home-item">
+      <h2>To implement</h2>
+      <h3>- Full test process (back & front)</h3>
+      <h3>- Fetch an allergens API</h3>
+      <h3>- Business logic for planification</h3>
+      <h3>- Include weather map</h3>
+      <h3>- Email alerts</h3>
     </div>
   </div>
 </template>
@@ -29,15 +113,52 @@ export default {
   justify-content: center;
   align-items: center;
   margin-top: var(--header-footer-height);
+  gap: 50px;
+  flex-wrap: wrap;
 }
 
-.title-item {
+.home-item {
   background-color: var(--color-background-item);
   border-radius: var(--default-radius);
   margin-top: 50px;
   padding: 20px;
   flex-basis: 500px;
-  flex-shrink: 1;
+}
+
+.logo-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+}
+
+.logo-item {
+  padding: 10px;
+}
+
+.logo-item img {
+  min-width: 60px;
+  height: 60px;
+}
+
+.api-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  flex-wrap: wrap;
+}
+
+.api-item {
+  padding: 10px;
+}
+
+.api-item img {
+  min-width: 150px;
+  height: 150px;
 }
 
 h1,
